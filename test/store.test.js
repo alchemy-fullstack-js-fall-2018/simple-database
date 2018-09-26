b3a61b90cfddb796eb1589e8df25254e73767254
Davidchhing <davidhchhing@gmail.com>
2018-09-26 14:11:23 -0700

    setting up my library and test folders

---
+++ test/store.test.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const assert = require('assert');
+const fs = require('fs');
+const store = require('../lib/store')
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
+
+describe('database that stores and retrieves objects from filesystem', () => {
+
+});
+
