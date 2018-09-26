04f9682ef2f46417e9b726e08917c854ff150b7e
Davidchhing <davidhchhing@gmail.com>
2018-09-26 16:46:57 -0700

    making first test, lots of bugs

--- test/store.test.js
+++ test/store.test.js
@@ -1,11 +1,38 @@
 const path = require('path');
 const assert = require('assert');
 const fs = require('fs');
-const store = require('../lib/store')
+const Store = require('../lib/store')
 const rimraf = require('rimraf')
 const mkdirp = require('mkdirp')
 
-describe('database that stores and retrieves objects from filesystem', () => {
+describe('stores and retrieves objects from filesystem', () => {
+
+    const rootPath = path.join(__dirname, 'base');
+    const store = new Store(rootPath);
+
+    beforeEach(done => {
+        rimraf(store, error => {
+            if(error && error.code !== 'ENOENT') done(error);
+            else done();
+        });
+    });
+    
+    beforeEach(() => {
+        mkdirp(rootPath);
+    });
+
+
+    it('this should save directory from root to destination with id', (done) => {
+        store.save({ fruits: 'apple' }), (error, fruit) => {
+            if(error) {
+                done(error);             
+            } else {
+                assert.ok(fruit._id);
+                done();
+            };
+        }
+    })
+
 
 });
 
---
+++ test/test.js
@@ -0,0 +1,10 @@
+function add (a, b) {
+  return a + b;
+}
+
+const a = 1;
+const b = 2;
+const c = 3;
+const d = 4;
+
+console.log(add(d,c));
