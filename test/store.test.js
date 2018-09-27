fbf1850e60e85e4785704b8dadb9f7ff86a0f9b9
KLPainter <KLPainter@gmail.com>
2018-09-27 10:21:24 -0700

    Revised tests based on specs

--- test/store.test.js
+++ test/store.test.js
@@ -1,54 +1,91 @@
 const assert = require('assert');
-const fs = require('fs');
 const mkdirp = require('mkdirp');
 const rimraf = require ('rimraf');
-const path = require ('path');
 const Store = require ('../lib/store');
 
 describe('simple database of JSON files', () => {
 
     const DATABASE_NAME = 'cats';
-
     const store = new Store (DATABASE_NAME);
 
     beforeEach(done => {
         rimraf(DATABASE_NAME, err => {
-            if(err && err.code !== 'ENOENT') done(err);
-            else done();
+            if(err && err.code !== 'ENOENT') return done(err);
+            mkdirp(DATABASE_NAME, function(err) {
+                if(err) return done(err);
+                done(err);
+            });
         });
     });
 
-    it.skip('gets existing file', (done) => {
-        store.get('luna.json', (err, objectFromFile) => {
-            if(err) return done(err);
-            assert.deepEqual(objectFromFile, { name:'Luna' });
-            done();
-        });
-    });
-    
-    it.skip('saves an object as a file', (done) => {
+    it.skip('saves an object as a file, then gets it by id', (done) => {
         const objectToSave = { name: 'Ember' };
         store.save (objectToSave, (err, objectThatSaved) => {
             if(err) return done(err);
-            assert.deepEqual(objectThatSaved, objectToSave);
-            done();
+            const hasId = objectThatSaved.hasOwnProperty('_id');
+            assert.deepEqual(hasId, true);
+            store.get(objectThatSaved._id, (err, objectFromFile) => {
+                if(err) return done(err);
+                assert.deepEqual(objectFromFile, objectThatSaved);
+                done();
+            });
         });
     });
-    
-    it.skip('removes an existing file', done => {
-        const fileToRemove = 'luna.json';
-        store.remove(fileToRemove, (err, statusObj) => {
+
+    it.skip('returns null when attempting to get file that doesn\'t exist', (done) => {
+        store.get('DoesNotExist', (err, objectFromFile) => {
             if(err) return done(err);
-            assert.deepEqual(statusObj, { removed: true });
+            assert.deepEqual(objectFromFile, null);
+            done();
+        });
+    });
+
+    it.skip('saves an object, removes it, then returns null when attempting to get it', (done) => {
+        const objectToSave = { name: 'Ember' };
+        store.save (objectToSave, (err, objectThatSaved) => {
+            if(err) return done(err);
+            store.remove(objectThatSaved._id, (err, statusObj) => {
+                if(err) return done(err);
+                assert.deepEqual(statusObj, { removed: true });
+                store.get('objectThatSaved._id', (err, objectFromFile) => {
+                    if(err) return done(err);
+                    assert.deepEqual(objectFromFile, null);
+                    done();
+                });
+            });
+        });
+    });
+
+    it.skip('returns false object when attempting to remove file that doesn\'t exist', done => {
+        store.remove('DoesNotExist', (err, statusObj) => {
+            if(err) return done(err);
+            assert.deepEqual(statusObj, { removed: false });
             done();
         });
     });
     
-    it('gets an array of all objects in the database', (done) => {
+    it.skip('getAll returns an empty array when no objects exist', (done) => {
         store.getAll((err, objects) => {
             if(err) return done(err);
-            assert.deepEqual(objects, [{ name: 'Ember' }, { name: 'luna' }])
+            assert.deepEqual(objects, []);
             done();
+        });
+    });
+
+    it('getAll returns an array of all objects in the store', (done) => {
+        store.save ({ name: 'Ember' }, (err, object1) => {
+            if(err) return done(err);
+            store.save ({ name: 'Luna' }, (err, object2) => {
+                if(err) return done(err);
+                store.save ({ name: 'Smooch' }, (err, object3) => {
+                    if(err) return done(err);
+                    store.getAll((err, objects) => {
+                        if(err) return done(err);
+                        assert.deepEqual(objects, [object1, object2, object3]);
+                        done();
+                    });
+                });
+            });
         });
     });
 
