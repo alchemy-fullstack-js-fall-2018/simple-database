d269aa5d4cb2d290fa758e95f8f0ff5579ffb363
Jack <ajackofsometrades@gmail.com>
2018-09-26 19:46:45 -0700

    red/green for getting files based on an array of input

--- test/store.test.js
+++ test/store.test.js
@@ -186,32 +186,35 @@ describe('store tests', () => {
 
     });
     
-    // describe('it gets an array of stuff', () => {
+    describe('it gets an array of stuff', () => {
 
-    //     it('returns an array of objects if the files exist', done => {
+        it('returns an array of objects if the files exist', done => {
 
-    //         const store = new Store(dbPath);
-    //         const objs = [        
-    //             { testing: [1, 1, 2, 3] }, 
-    //             { testing: [9, 9, 8, 7] }
-    //         ];
-    //         let ids = [];
+            const store = new Store(dbPath);
+            const objs = [        
+                { testing: [1, 1, 2, 3] }, 
+                { testing: [9, 9, 8, 7] }
+            ];
+            const ids = [];
+            let count = objs.length;
 
-    //         objs.forEach(obj => {
-    //             store.save(obj, (err, objSaved) => {
-    //                 if(err) return done(err);
-    //                 ids.push(objSaved._id);
-    //             });
-    //         });
-
-    //         store.get(ids, (err, objFile) => {
-    //             if(err) return done(err);
-    //             assert.deepEqual(objFile.testing, obj.testing);
-    //             done();
-    //         });
-    //     });
-
-
-    // });
+            objs.forEach((obj) => {
+                store.save(obj, (err, objSaved) => {
+                    if(err) return done(err);
+                    ids.push(objSaved._id);
+                    count--;
+                    if(count == 0) {
+                        if(ids.length === objs.length) {
+                            store.getList(ids, (err, objArr) => {
+                                if(err && err.code !== 'ENOENT') return done(err);
+                                assert.deepEqual(objArr.length, ids.length);
+                                done();
+                            });
+                        }                    
+                    }
+                });
+            });
+        });
+    });
 
 });
