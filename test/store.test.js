a9c50e08c0efda6e0cf9a42af85a65ee8f1a8838
Davidchhing <davidhchhing@gmail.com>
2018-09-26 18:07:43 -0700

    save test finally finally works!!

--- test/store.test.js
+++ test/store.test.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const assert = require('assert');
-const fs = require('fs');
-const Store = require('../lib/store')
-const rimraf = require('rimraf')
-const mkdirp = require('mkdirp')
+// const fs = require('fs');
+const Store = require('../lib/store');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 
 describe('stores and retrieves objects from filesystem', () => {
 
@@ -11,28 +11,27 @@ describe('stores and retrieves objects from filesystem', () => {
     const store = new Store(rootPath);
 
     beforeEach(done => {
-        rimraf(store, error => {
+        return rimraf(rootPath, error => {
             if(error && error.code !== 'ENOENT') done(error);
             else done();
         });
     });
     
     beforeEach(() => {
-        mkdirp(rootPath);
+        return mkdirp(rootPath);
     });
 
-
     it('this should save directory from root to destination with id', (done) => {
-        store.save({ fruits: 'apple' }), (error, fruit) => {
+        store.save({ fruits: 'apple' }, (error, fruit) => {
             if(error) {
                 done(error);             
             } else {
                 assert.ok(fruit._id);
                 done();
-            };
-        }
-    })
 
-
+                // const file = fs.readFileSync(path.join(rootPath, `${fruits._id}.json`, 'utf8'));
+            }
+        });
+    });
 });
 
