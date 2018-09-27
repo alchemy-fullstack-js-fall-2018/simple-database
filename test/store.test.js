8daffb64ae38001b77712fc1c3d91100f997c5a5
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-09-27 09:35:03 -0700

    get all working

--- test/store.test.js
+++ test/store.test.js
@@ -98,4 +98,34 @@ describe('store', () => {
             }); 
         });
     });
+
+    it('returns an array of objects if the files exist', done => {
+        const store = new Store(databasePath);
+        const items = [        
+            { test: 'sloth' }, 
+            { test: 'cheetah' }
+        ];
+        items.forEach(item => {
+            store.save(item, (err) => {
+                if(err) return done(err);
+            });
+        });
+        store.getAll((err, itemArr) => {
+            if(err) return done(err);
+            assert.equal(itemArr.length, items.length);
+            done();
+        });
+    });
+
+    it('returns an empty array if files do not exist', done => {
+        const store = new Store(databasePath);
+        store.getAll((err, itemArr) => {
+            if(err) return done(err);
+            assert.equal(itemArr.length, 0);
+            done();
+        });
+        
+
+    }); 
+    
 });
