606d01ed3d6ee48859b560e06a15ec9215f519c4
Sophie Graham <sophiergraham1@gmail.com>
2018-09-27 11:15:26 -0700

    get test failing, still need to write function

--- test/store.test.js
+++ test/store.test.js
@@ -1,24 +1,39 @@
 const assert = require('assert');
 const Store = require('../lib/store');
+const path = require('path');
+const fs = require('fs');
 
 describe('store', () => {
-    const dbPath = path.join(_dirname, 'dinosaurs');
+    const dbPath = path.join(__dirname, 'dinosaurs');
 
-    it('adds id to object', () => {
-        const store = new Store();
+    it('adds id to object', done => {
+        const store = new Store(path.join(dbPath));
         store.save({ name: 'little foot' }, (err, dinosaur) => {
             if(err) return done(err);
-        });
-        assert.ok(object.id);
-    });
-    it('saves object to a file', () => {
-        const store = new Store(dbPath);
-        store.save({ name: 'little foot' }, (err, dinosaur) => {
-            if(err) return done(err);
-            console.log('name', name);
-            const file = fs.readFilerSync(path.join{dbPath, `${name._id}.JSON`}, 'utf8');
-            assert.equal(JSON.parse(file).name, 'little foot');
+            assert.ok(dinosaur._id);
             done();
         });
+    });
+
+    it('saves object to a file', done => {
+        const store = new Store(path.join(dbPath));
+        store.save({ name: 'little foot' }, (err, dinosaur) => {
+            if(err) return done(err);
+            const file = fs.readFileSync(path.join(dbPath, `${dinosaur._id}.json`), 'utf8');
+            assert.equal(dinosaur.name, JSON.parse(file).name);
+            done();
+        });
+    });
+
+    it('takes an error and deserialize', done => {
+        const store = new Store(path.join(dbPath));
+        store.get('_-LlzgkyC', (err, dinosaur) => {
+            if(err) return done(err);
+            const file = fs.readFileSync(path.join(dbPath, '_-LlzgkyC.json'), 'utf8');
+            assert.deepEqual(dinosaur, JSON.parse(file));
+
+        });
+
+
     });
 });
