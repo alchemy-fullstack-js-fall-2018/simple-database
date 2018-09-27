06e15d89d23a1d39794bd2e824f416a068801c78
MikeBLambert <mlamb128@gmail.com>
2018-09-27 12:47:14 -0700

    Progress towards removing and recreating file at beginning of test suite

--- test/store.test.js
+++ test/store.test.js
@@ -1,15 +1,35 @@
 const assert = require('assert');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 const Store = require('../lib/store');
-// const rimraf = require('rimraf');
+
+const dbPath = path.join(__dirname, 'book-db');
 
 
-describe('save file', () => {
-
-    const dbPath = path.join(__dirname, 'book-dir-dest');
+describe('store', () => {
     
-    it('book has an id property', done => {
+    beforeEach(done => {
+
+        rimraf(dbPath, err => {
+            if(err && err.code === 'ENOENT') {
+                mkdirp(dbPath, err => {
+                    if(err) done(err);
+                    done();  
+                });
+            }
+            
+            // done(err) ;
+            mkdirp(dbPath, err => {
+                if(err) done(err);
+                done();
+            })
+        // });
+
+    });
+    
+    it('saves a file that has an id property', done => {
         const store = new Store(dbPath);
         store.save({ title: 'Moby Dick' }, (err, newBook) => {
             if(err) return done(err);
@@ -29,4 +49,55 @@ describe('save file', () => {
         });
     });
     
+    it('gets file when passed an id', done => {
+        const store = new Store(dbPath);
+        
+        store.save({ title: 'Moby Dick' }, (err, newBook) => {
+            if(err) return done(err);
+            
+            store.get(newBook.id, (err, existingBook) => {
+                if(err) return done(err);
+                assert.equal(existingBook.id, newBook.id);
+                done();
+            });
+
+        });
+    });
+
+    it('gets null when incorrect id is passed', done => {
+        const store = new Store(dbPath);
+        store.get('iAmNotARealFile', (err, existingBook) => {
+            if(err) return done(err);
+            assert.equal(existingBook, null);
+            done();
+        });
+    });
+    
+    it('removes file when passed a valid id', done => {
+        const store = new Store(dbPath);
+
+        store.save({ title: 'Moby Dick' }, (err, newBook) => {
+            if(err) return done(err);
+
+            store.remove(newBook.id, (err, removeObj) => {
+                if(err) return done(err);
+                assert.equal(removeObj.removed, true);
+                done();
+            });
+
+        });
+    });
+
+    it('gets all objects in database', done => {
+        const store = new Store(dbPath);
+
+        const dbContents = fs.readdir(dbPath);
+        
+        store.getAll((err, arrOfObjs) => {
+            if(err) done(err);
+            assert.equal(arrOfObjs, dbContents);
+        });
+    });
+
 });
+
