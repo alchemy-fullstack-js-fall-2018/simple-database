76e8d89cb27b1435f008f6f4a5dab86a5c14987b
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-27 10:36:01 -0700

    1st test for remove failing

--- test/store.test.js
+++ test/store.test.js
@@ -56,5 +56,27 @@ describe('creates file', () => {
         });
 
     });
+    
+    describe('remove method', () => {
+
+        it('removes a file with provided id', done => {
+            const store = new Store(dbPath);
+            store.get('afgo9GhVJS', (err, animal) => {
+                if(err) return done(err);
+                assert.deepEqual(animal.animal, 'cat');
+                done();
+            });
+        });
+
+        it('returns null if no file with id exists', done => {
+            const store = new Store(dbPath);
+            store.get('_afAiAh191', (err, status) => {
+                if(err) return done(err);
+                assert.deepEqual(status.removed, true);
+                done();
+            });
+        });
+
+    });
 
 });
