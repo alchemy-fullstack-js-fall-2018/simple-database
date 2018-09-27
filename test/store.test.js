6ca7230ca059a7bb0f08a7cc2c63db8e6bca32e6
Ryan Luras <ryanluras@gmail.com>
2018-09-27 09:27:18 -0700

    remove working correctly

--- test/store.test.js
+++ test/store.test.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-//const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
@@ -12,8 +11,7 @@ describe('Store Database', () => {
 
     beforeEach(done => {
         return rimraf(rootDirectory, err => {
-            if(err && err.code !== 'ENOENT') done(err);
-            else done();
+            err && err.code !== 'ENOENT' ? done(err) : done();
         });
     });
 
@@ -23,11 +21,45 @@ describe('Store Database', () => {
 
     it('saves a file to the database with a shortid', done => {
         store.save({ file: 'file contents' }, (err, object) => {
-            if(err) return done(err);
-            assert.ok(object._id);
+            err ? done(err) : assert.ok(object._id);
             done();
-
         });
-
     });
+
+    it('gets file by id', done => {
+        store.save({ file: 'file contents' }, (err, object) => {
+            if(err) return done(err);
+            done();
+            store.get((object._id), (err, objectFromFile) => {
+                err ? done(err) : assert.deepEqual(object.id, objectFromFile);
+                done();
+            });
+        });
+    });
+
+    it('returns null if no object exists', done => {
+        store.get(('fake-file'), (err) => {
+            err !== null ? done(err) : assert.equal(err, null);
+            done();
+        });
+    });
+
+    it('removes a file by id and returns true', done => {
+        store.save({ file: 'file contents' }, (err, object) => {
+            err ? done(err) : done();
+            store.remove(object._id, (err, objectFromFile) => {
+                if(err) return done(err);
+                assert.equal(objectFromFile.removed, true);
+                done();
+            }); 
+        });
+    });
+
+    it('removes a file and returns false if does not exist', done => {
+        store.remove(null, (err, objectFromFile) => {
+            err ? done(err) : assert.equal(objectFromFile.removed, false);
+            done();
+        }); 
+    });
+
 });
