75d3e66179bea6b6ddc7770f40b0b0fff3742b98
Alex Rankin <alex.c.rankin@gmail.com>
2018-09-26 17:33:53 -0700

    re-did tests and lib to work with callbacks

--- tests/store.test.js
+++ tests/store.test.js
@@ -3,10 +3,25 @@ const fs = require('fs');
 const path = require('path');
 const Store = require('../lib/store');
 
+const dbPath = path.join(__dirname, 'sandwiches')
+
 describe('save', () => {
-    it('should add an id to a sandwich', () => {
-        let store = new Store();
-        const sandwich = store.save({ name: 'Reuben' });        
-        assert.ok(sandwich._id);
+    it('should add an id to a sandwich', done => {
+        let store = new Store(dbPath);
+        store.save({ name: 'Reuben' }, (err, sandwich) => {
+            if(err) return done(err);
+            assert.ok(sandwich._id);
+            done();
+        });        
+    });
+
+    it('should write the object to a file', done => {
+        let store = new Store(dbPath);
+        store.save({ name: 'Hoagie' }, (err, sandwich) => {
+            if(err) return done(err);
+            const file = fs.readFileSync(path.join(dbPath, `${sandwich._id}.json`), 'utf8');
+            
+            assert.equal(sandwich, );
+        });
     });
 });
