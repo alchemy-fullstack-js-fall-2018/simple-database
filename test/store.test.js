bb1606378615e8ffb2cee9ac4f8976b9ef04144e
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-09-26 17:04:25 -0700

    added to json for watch and get tests working

--- test/store.test.js
+++ test/store.test.js
@@ -26,16 +26,39 @@ describe('store', () => {
     });
     
 
-    it('save a file to animals with id', done => {
+    it('saves a file to simpleDB with _id', done => {
         const store = new Store(databasePath);
         store.save({ 'test':'sloth' }, (err, item) => {
             if(err) return done(err);
 
             const readFile = path.join(databasePath, `${item._id}.json`);
             fs.readFileSync(readFile);
+
             assert.ok(item._id);
             assert.deepEqual({ '_id': item._id, 'test':'sloth' }, item);
             done();
         });
+    });
+
+    it('gets a object from file by _id', done => {
+        const store = new Store(databasePath);
+        store.save({ 'test':'sloth' }, (err, itemSaved) => {
+            if(err) return done(err);
+            store.get(itemSaved._id, (err, item) => {
+                assert.ok(item._id);
+                assert.deepEqual({ '_id': item._id, 'test':'sloth' }, item);
+                done();
+            }); 
+        });
+    });
+
+    it('returns null if file does not exist', done => {
+        const store = new Store(databasePath);
+        store.get('badId', (err, item) => {
+            if(err && err.code !== 'ENOENT') return done(err);
+            assert.equal(null, item);
+            done();
+        });
+
     });
 });
