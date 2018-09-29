81ee8e23ef72c65bccccda79a003a67dfd7153e6
Sophie Graham <sophiergraham1@gmail.com>
2018-09-28 17:26:51 -0700

    get test and function passing

--- test/store.test.js
+++ test/store.test.js
@@ -1,10 +1,24 @@
 const assert = require('assert');
 const Store = require('../lib/store');
 const path = require('path');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 const fs = require('fs');
 
 describe('store', () => {
     const dbPath = path.join(__dirname, 'dinosaurs');
+
+    beforeEach(done => {
+        rimraf(dbPath, err => {
+            if(err && err.code !== 'ENOENT') done(err);
+            else {
+                mkdirp(dbPath, err => {
+                    if(err && err.code !== 'ENOENT') done(err);
+                    else done();
+                });
+            }
+        });
+    });
 
     it('adds id to object', done => {
         const store = new Store(path.join(dbPath));
@@ -17,20 +31,18 @@ describe('store', () => {
 
     it('saves object to a file', done => {
         const store = new Store(path.join(dbPath));
+
         store.save({ name: 'little foot' }, (err, dinosaur) => {
             if(err) return done(err);
             const file = fs.readFileSync(path.join(dbPath, `${dinosaur._id}.json`), 'utf8');
             assert.equal(dinosaur.name, JSON.parse(file).name);
-            done();
+
+            store.get(dinosaur._id, (err, getDinosaur) => {
+                if(err) return done(err);
+                assert.deepEqual(getDinosaur, JSON.parse(file));
+                done();
+            }); 
         });
     });
 
-    it('takes an error and deserialize', done => {
-        const store = new Store(path.join(dbPath));
-        store.get('_-LlzgkyC', (err, dinosaur) => {
-            if(err) return done(err);
-            const file = fs.readFileSync(path.join(dbPath, '_-LlzgkyC.json'), 'utf8');
-            assert.deepEqual(dinosaur, JSON.parse(file));
-        });
-    });
 });
