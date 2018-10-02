e2102922b5af19ed806fcdfaea89e358537978ad
MikeBLambert <mlamb128@gmail.com>
2018-10-01 17:59:58 -0700

    getAll working when run independently. There appears to be a bug in rimraf that is generating an ENOENT or EPERM error

--- test/store.test.js
+++ test/store.test.js
@@ -11,18 +11,21 @@ const dbPath = path.join(__dirname, 'book-db');
 describe('store', () => {
     
     beforeEach(done => {
-
         rimraf(dbPath, err => {
-            if(err && err.code !== 'ENOENT') return done(err);
+            if(err && err.code !== 'ENOENT') done(err);
+            done();
+        });
 
-            mkdirp(dbPath, err => {
-                if(err) return done(err);
-                done();
-            });
-        
+    });
+
+    beforeEach(done => {
+        mkdirp(dbPath, err => {
+            if(err) return done(err);
+            done();
         });
     });
     
+
     it('saves a file that has an id property', done => {
         const store = new Store(dbPath);
         store.save({ title: 'Moby Dick' }, (err, book) => {
@@ -40,6 +43,7 @@ describe('store', () => {
 
             const readFile = path.join(dbPath, `${book.id}.json`);
             const destContents = fs.readFileSync(readFile, 'utf8');
+
             assert.equal(JSON.parse(destContents).title, 'Moby Dick');
             done();
         });
@@ -50,13 +54,14 @@ describe('store', () => {
         
         store.save({ title: 'Moby Dick' }, (err, book) => {
             if(err) return done(err);
-            
+
+            // const file = JSON.parse(fs.readFileSync(path.join(dbPath, `${book.id}.json`)), 'utf8');
+
             store.get(book.id, (err, existingBook) => {
                 if(err) return done(err);
                 assert.deepEqual(existingBook, book);
                 done();
             });
-
         });
     });
 
@@ -77,7 +82,9 @@ describe('store', () => {
         store.save({ title: 'Moby Dick' }, (err, book) => {
             if(err) return done(err);
 
-            store.remove(book.id, (err, removeObj) => {
+            const file = JSON.parse(fs.readFileSync(path.join(dbPath, `${book.id}.json`)), 'utf8');
+
+            store.remove(file.id, (err, removeObj) => {
                 if(err) return done(err);
                 assert.equal(removeObj.removed, true);
                 done();
@@ -86,21 +93,34 @@ describe('store', () => {
         });
     });
 
-    it.only('gets all objects in database', done => {
+    it('gets all objects in database', done => {
         const store = new Store(dbPath);       
         
         store.save({ title: 'Moby Dick' }, (err, book1) => {
             if(err) return done(err);
 
+            const file1 = JSON.parse(fs.readFileSync(path.join(dbPath, `${book1.id}.json`)), 'utf8');
+
+
             store.save({ title: 'Hunger Games' }, (err, book2) => {
                 if(err) return done(err);
 
+                const file2 = JSON.parse(fs.readFileSync(path.join(dbPath, `${book2.id}.json`)), 'utf8');
+
                 store.getAll((err, arrOfObjs) => {
-                    if(err) done(err);
-                    assert.equal(arrOfObjs, [book1, book2]);
+                    if(err) return done(err);
+
+                    const expected = [file1, file2].sort((a, b) => {
+                        if(a.id > b.id) return 1;
+                        if(a.id < b.id) return -1;
+                        return 0;
+                    });
+
+                    assert.deepEqual(arrOfObjs, expected);
+                    done();
                 });
             });
-        });
+        }); 
     });
 });
 
