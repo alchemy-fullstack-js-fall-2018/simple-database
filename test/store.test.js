af1be064668c860bfde37de7c7c9f8f389d18730
Claire Flanagan <claireflanagan@gmail.com>
2018-10-03 21:42:10 -0700

    remove test passing

--- test/store.test.js
+++ test/store.test.js
@@ -29,12 +29,43 @@ describe('save', () => {
 
     it('gets a whale??', done => {
         const store = new Store(path.join(dbPath));
-        store.get('_P3o3FtkPt', (error, whale) => {
+        store.get('_0bzJV7lsB', (error, whale) => {
             if(error) return done(error);
-            const file = fs.readFileSync(path.join(dbPath, '_P3o3FtkPt.json'), 'utf8');
+            const file = fs.readFileSync(path.join(dbPath, '_0bzJV7lsB.json'), 'utf8');
             assert.deepEqual(whale, JSON.parse(file));
             done();
         });
     });
+
+    it('removes a whale', done => {
+        const store = new Store(path.join(dbPath));
+        store.save({ species: 'Blue Whale' }, (error, whale) => {
+            if(error) return done(error);
+
+            store.remove(whale._id, (error, statusObj) => {
+                if(error) return done(error);
+                assert.deepEqual(statusObj, { removed: true });
+            });
+            done();
+        });
+    });
+
+    // it('returns an array of all whales', done => {
+    //     const store = new Store(path.join(dbPath));
+    //     store.save({ species: 'Green Whale' }, (error, greenWhale) => {
+    //         if(error) return done(error);
+    //     });
+    //     store.save({ species: 'Purple Whale' }, (error, purpleWhale) => {
+    //         if(error) return done(error);
+    //     });
+    //     store.getAll((err, whales) => {
+    //         if(err) return done(err);
+    //         assert.deepEqual(whales, [{ species: 'Green Whale' }, { species: 'Purple Whale' }])
+    //     });
+    // });
+
+
+
+
 
 });
